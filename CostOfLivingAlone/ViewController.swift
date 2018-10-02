4e57ee2958adb063842d919b0151bda2e4128ebb
YoshiakiTanaka <pmwt12@gmail.com>
2018-10-02 14:38:29 +0900

    Initial Commit

---
+++ CostOfLivingAlone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CostOfLivingAlone
+//
+//  Created by 田中良明 on 2018/10/02.
+//  Copyright © 2018年 tanakayoshiaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
